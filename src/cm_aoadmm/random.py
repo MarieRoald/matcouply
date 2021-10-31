0c07ca2529e0e0a46975b709a0917465d4c3d97b
Marie Roald <roald.marie@gmail.com>
2021-10-31 12:11:36 +0100

    Added random generation of decompositions

---
+++ src/cm_aoadmm/random.py
@@ -0,0 +1,51 @@
+import tensorly as tl
+from .coupled_matrices import CoupledMatrixFactorization
+
+
+def random_coupled_matrices(shapes, rank, full=False, random_state=None,
+                            normalise_factors=True, normalise_B=False, **context):
+    """Generate a random coupled matrix decomposition
+
+    Parameters
+    ----------
+    shapes : tuple
+        A tuple where each element represents the shape of a matrix
+        represented by the coupled matrix decomposition model. The
+        second element in each shape-tuple must be constant.
+    rank : int or int list
+        rank of the coupled matrix decomposition
+    full : bool, optional, default is False
+        if True, a full tensor is returned otherwise,
+        the decomposed tensor is returned
+    random_state : `np.random.RandomState`
+    """
+    rns = tl.check_random_state(random_state)
+    if not all(shape[1] == shapes[0][1] for shape in shapes):
+        raise ValueError('All matrices must have equal number of columns.')
+
+    A = rns.random_sample((len(shapes), rank), **context)
+    B_is = [rns.random_sample((j_i, rank), **context) for j_i, k in shapes]
+    K = shapes[0][1]
+    C = rns.random_sample((K, rank), **context)
+
+    weights = tl.ones(rank, **context)
+
+    if normalise_factors or normalise_B:
+        B_i_norms = [tl.norm(B_i, axis=0) for B_i in B_is]
+        B_is = [B_i / B_i_norm for B_i, B_i_norm in zip(B_is, B_i_norms)]
+        B_i_norms = tl.stack(B_i_norms)
+        
+        A = A * B_i_norms
+    if normalise_factors:
+        A_norm = tl.norm(A, axis=0)
+        A = A / A_norm
+
+        C_norm = tl.norm(C, axis=0)
+        C = C / C_norm
+        weights = A_norm*C_norm
+
+    cmf = CoupledMatrixFactorization((weights, (A, B_is, C)))
+    if full:
+        return cmf.to_matrices()
+    else:
+        return cmf
---
+++ tests/test_random.py
@@ -0,0 +1,49 @@
+import tensorly as tl
+from tensorly.testing import assert_equal, assert_array_almost_equal
+from cm_aoadmm.random import random_coupled_matrices
+from numpy.linalg import matrix_rank
+import pytest
+import numpy as np
+
+
+def test_random_coupled_matrices():
+    """test for random.random_coupled_matrices"""
+    shapes = [(10, 11), (12, 11), (9, 11), (10, 11), (15, 11)]
+    rank = 4
+
+    # Check that assembled matrices have correct shapes and ranks
+    coupled_matrices = random_coupled_matrices(shapes, rank, full=True)
+    
+    assert len(coupled_matrices) == len(shapes)
+    for matrix, shape in zip(coupled_matrices, shapes):
+        assert tl.shape(matrix) == shape
+        assert matrix_rank(matrix) == rank
+
+    # Check that factor matrices have correct shape
+    weights, (A, B_is, C) = random_coupled_matrices(shapes, rank, full=False)
+    assert tl.shape(A) == (len(shapes), rank)
+    assert all(tl.shape(B_i) == (J_i, rank) for B_i, (J_i, K) in zip(B_is, shapes))
+    assert tl.shape(C) == (shapes[0][1], rank)
+
+
+    
+    # Check that normalising B_is gives B_is with unit normed columns
+    weights, (A, B_is, C) = random_coupled_matrices(shapes, rank, full=False, normalise_B=True, normalise_factors=False)
+    for B_i in B_is:
+        assert_array_almost_equal(tl.norm(B_i, axis=0), tl.ones(rank))
+    assert_array_almost_equal(weights, tl.ones(rank))
+
+    # Check that normalising all factors gives factor matrices with unit normed columns
+    weights, (A, B_is, C) = random_coupled_matrices(shapes, rank, full=False, normalise_factors=True)
+    assert_array_almost_equal(tl.norm(A, axis=0), tl.ones(rank))
+    for B_i in B_is:
+        assert_array_almost_equal(tl.norm(B_i, axis=0), tl.ones(rank))
+    assert_array_almost_equal(tl.norm(C, axis=0), tl.ones(rank))
+
+    # Should fail when shapes have different value for the number of columns
+    shapes =  [(10, 10), (12, 11), (9, 11), (10, 11), (15, 11)]
+    with pytest.raises(ValueError):
+        random_coupled_matrices(shapes, rank)
+    
+    
+    
