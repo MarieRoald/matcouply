da6bf5d25387f1c15549d5833ed20c3f58f79fb3
Marie Roald <roald.marie@gmail.com>
2021-11-02 18:35:09 +0100

    Added tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,61 @@
+from numpy.core.numeric import allclose
 from cm_aoadmm import _utils as utils
+import numpy as np
+import scipy
+import pytest
 
 
 def test_is_iterable():
-    # TODO: Make this test. 
-    # TESTPLAN:
-    # Use a list, tuple, str, generator and class with the __iter__ function
-    # Test with some stuff that is not iterable too (int, float, class without __iter__)
-    pass
-    
+    # Test with some objects that are iterable
+    class TestIterable:
+        def __iter__(self):
+            return self
 
-def test_get_svd(rng):
-    # TODO: Make this test. 
-    # TESTPLAN:
-    # Create random matrix, X
-    # Compute SVD with both scipy.linalg.svd (U1, s1, Vh1) and utils.get_svd called with svd="truncated_svd" and svd="numpy_svd" (U2, s2, Vh2)
-    # Compute U1.T@U2 and Vh1.T@Vh2
-    # Assert that U1.T@U2 == Vh1.T@Vh2
-    # Assert that U1.T@U2 * Vh1.T@Vh2 == identity
-    # Assert that s1 == s2
-    pass
+        def __next__(self):
+            return 1
+
+    test_iterables = [
+        [1, 2, 3, 4],
+        (1, 2, 3, 4),
+        "iterablestring",
+        {1: "value1", 2: "value2"},
+        range(5),
+        TestIterable()
+    ]
+    for test_iterable in test_iterables:
+        assert utils.is_iterable(test_iterable)
+
+    # Test with some objects that arent't iterable
+    def test_function(x): return x
+    class TestNotIterable():
+        pass
+    test_not_iterables = [
+        1,
+        3.14,
+        test_function,
+        TestNotIterable()
+    ]
+    for test_not_iterable in test_not_iterables:
+        assert not utils.is_iterable(test_not_iterable)
+
+
+@pytest.mark.parametrize("svd", ["numpy_svd", "truncated_svd"])
+def test_get_svd(rng, svd):
+    X = rng.standard_normal(size=(10, 20))
+    U1, s1, Vh1 = scipy.linalg.svd(X)
+    svd_fun = utils.get_svd(svd)
+    U2, s2, Vh2 = svd_fun(X)
+
+    # Check singular values are the same
+    assert allclose(s1, s2)
+    
+    # Check that first 10 (rank) singular vectors are equal or flipped
+    U1TU2 = U1.T@U2
+    Vh1Vh2T = Vh1@Vh2.T
+    Vh1Vh2T = Vh1Vh2T[:10, :10]
+    assert allclose(U1TU2, Vh1Vh2T)
+    assert allclose(U1TU2 * Vh1Vh2T, np.eye(U1TU2.shape[0]))
+
+def test_get_svd_fails_with_invalid_svd_name():
+    with pytest.raises(ValueError):
+        utils.get_svd("THIS_IS_NOT_A_VALID_SVD")
