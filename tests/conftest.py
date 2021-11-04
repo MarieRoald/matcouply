5d374629c904615f0a5a0b2456d46311f029cf1c
Marie Roald <roald.marie@gmail.com>
2021-11-04 19:15:00 +0100

    Updated fixtures so cmfs are generated with sufficiently small rank

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,19 +30,17 @@ def random_regular_shapes(rng):
     K = rng.randint(1, 20)
     shapes = tuple((J, K) for i in range(I))
     return shapes
-    
-@pytest.fixture
-def random_rank(rng):
-    rank = rng.randint(1, 6)
-    return rank
 
 @pytest.fixture
-def random_ragged_cmf(rng, random_ragged_shapes, random_rank):
-    cmf = random_coupled_matrices(random_ragged_shapes, random_rank, random_state=rng)
-    return cmf, random_ragged_shapes, random_rank
+def random_ragged_cmf(rng, random_ragged_shapes,):
+    smallest_J = min(shape[0] for shape in random_ragged_shapes)
+    rank = rng.randint(1, smallest_J+1)
+    cmf = random_coupled_matrices(random_ragged_shapes, rank, random_state=rng)
+    return cmf, random_ragged_shapes, rank
 
 
 @pytest.fixture
-def random_regular_cmf(rng, random_regular_shapes, random_rank):
-    cmf = random_coupled_matrices(random_regular_shapes, random_rank, random_state=rng)
-    return cmf, random_regular_shapes, random_rank
+def random_regular_cmf(rng, random_regular_shapes,):
+    rank = rng.randint(1, random_regular_shapes[0][0])
+    cmf = random_coupled_matrices(random_regular_shapes, rank, random_state=rng)
+    return cmf, random_regular_shapes, rank
