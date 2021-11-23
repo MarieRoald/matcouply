e6e9667aa52163f9e4048b9c410e6a26e88249c1
Marie Roald <roald.marie@gmail.com>
2021-11-23 20:07:11 +0100

    Updated to support plotly

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import plotly.io as pio
 
+pio.renderers.default = "sphinx_gallery"
 
 # -- Project information -----------------------------------------------------
 
