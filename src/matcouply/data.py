d184906c65b058e6afac033f92099ce0d5e9953c
Marie Roald <roald.marie@gmail.com>
2021-12-04 17:39:07 +0100

    Added semiconductor etch dataset

--- src/matcouply/data.py
+++ src/matcouply/data.py
@@ -1,13 +1,19 @@
 import json
 from datetime import datetime, timedelta
+from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
 
 import pandas as pd
+import requests
+from scipy.io import loadmat
+from tqdm import tqdm
 
 DATASET_PARENT = Path(__file__).parent / "datasets"
+DOWNLOADED_PARENT = DATASET_PARENT / "downloads"
 
 
+# TODO: Convert to xarray datasets
 def get_bike_data():
     r"""Get bike sharing data from three major Norwegian cities
 
@@ -60,3 +66,126 @@ def get_bike_data():
     out["station_metadata"].index = out["station_metadata"].index.astype(int)
 
     return out
+
+
+def get_raw_semiconductor_etch_data(download_data=True, save_data=True):
+    """Load semiconductor etch data from :cite:p:`wise1999comparison`.
+
+    If the dataset is already downloaded on your computer, then the local files will be
+    loaded. Otherwise, they will be downloaded. By default, the files are downloaded from
+    https://eigenvector.com/data/Etch.
+
+    Parameters
+    ----------
+    download_data : bool
+        If ``False``, then an error will be raised if the data is not
+        already downloaded.
+    save_data : bool
+        if ``True``, then the data will be stored locally to avoid having to download
+        multiple times.
+
+    Returns
+    -------
+    dict
+        Dictionary where the keys are the dataset names and the values are the contents
+        of the MATLAB files.
+    """
+    data_urls = {
+        "MACHINE_Data.mat": "http://eigenvector.com/data/Etch/MACHINE_Data.mat",
+        "OES_DATA.mat": "http://eigenvector.com/data/Etch/OES_DATA.mat",
+        "RFM_DATA.mat": "http://eigenvector.com/data/Etch/RFM_DATA.mat",
+    }
+    data_raw_mat = {}
+
+    print("Loading semiconductor etch data from Wise et al. (1999) - J. Chemom. 13(3‐4), pp.379-396.")
+    print("The data is available at: http://eigenvector.com/data/Etch/")
+    for file, url in tqdm(data_urls.items()):
+        file_path = DOWNLOADED_PARENT / file
+        if file_path.exists():
+            data_raw_mat[file] = loadmat(file_path)
+        elif download_data:
+            request = requests.get(url)
+            if request.status_code != 200:
+                raise ValueError(f"Cannot download file {url} - Response: {request.status_code} {request.reason}")
+
+            if save_data:
+                DOWNLOADED_PARENT.mkdir(exist_ok=True, parents=True)
+                with open(file_path, "wb") as f:
+                    f.write(request.content)
+
+            data_raw_mat[file] = loadmat(BytesIO(request.content))
+        else:
+            raise ValueError("The semiconductor etch data is not yet downloaded, and ``download_data=False``.")
+    return data_raw_mat
+
+
+def get_semiconductor_etch_machine_data(download_data=True, save_data=True):
+    """Load machine measurements from the semiconductor etch dataset from :cite:p:`wise1999comparison`.
+
+    This function will load the semiconductor etch machine data and prepare it for analysis.
+
+    If the dataset is already downloaded on your computer, then the local files will be
+    loaded. Otherwise, they will be downloaded. By default, the files are downloaded from
+    https://eigenvector.com/data/Etch.
+
+    Parameters
+    ----------
+    download_data : bool
+        If ``False``, then an error will be raised if the data is not
+        already downloaded.
+    save_data : bool
+        if ``True``, then the data will be stored locally to avoid having to download
+        multiple times.
+
+    Returns
+    -------
+    dict
+        Dictionary where the keys are the dataset names and the values are the contents
+        of the MATLAB files.
+    """
+    # Get raw MATLAB data and parse into Python dict
+    data = get_raw_semiconductor_etch_data(download_data=download_data, save_data=save_data)["MACHINE_Data.mat"][
+        "LAMDATA"
+    ]
+    data = {key: data[key].squeeze().item().squeeze() for key in data.dtype.fields}
+
+    # Format data as xarray dataset
+    varnames = data["variables"][2:]
+
+    # Get the training data
+    train_names = [name.split(".")[0][1:] for name in data["calib_names"]]
+    train_data = {
+        name: pd.DataFrame(Xi[:-1, 2:], columns=varnames)  # Slice away last row since it "belongs" to the next sample
+        for name, Xi in zip(train_names, data["calibration"])
+    }
+    train_metadata = {}
+    for i, name in enumerate(list(train_data)):
+        train_data[name].index.name = "Time point"
+        train_data[name].columns.name = "Measurement"
+
+        metadata = pd.DataFrame(data["calibration"][i][:-1, [0, 1]], columns=["Time", "Step number"])
+        metadata["Experiment"] = int(name[:2])
+        metadata["Sample"] = int(name[2:])
+        metadata.index.name = "Time point"
+        metadata.columns.name = "Metadata"
+        train_metadata[name] = metadata
+
+    # Get the testing data
+    test_names = [name.split(".")[0][1:] for name in data["test_names"]]
+    test_data = {
+        name: pd.DataFrame(Xi[:-1, 2:], columns=varnames)  # Slice away last row since it "belongs" to the next sample
+        for name, Xi in zip(test_names, data["test"])
+    }
+    test_metadata = {}
+    for i, name in enumerate(list(test_data)):
+        test_data[name].index.name = "Time point"
+        test_data[name].columns.name = "Measurement"
+
+        metadata = pd.DataFrame(data["test"][i][:-1, [0, 1]], columns=["Time", "Step number"])
+        metadata["Experiment"] = int(name[:2])
+        metadata["Sample"] = int(name[2:])
+        metadata.index.name = "Time point"
+        metadata.columns.name = "Metadata"
+        test_metadata[name] = metadata
+
+    return train_data, train_metadata
