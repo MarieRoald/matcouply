e651a0722c2a224952baa403adcd33758b5aa1c1
Marie Roald <roald.marie@gmail.com>
2022-08-14 11:50:23 +0200

    Bump version: 0.1.1 → 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2022, Marie Roald"
 author = "Marie Roald"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.1"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
