9087472bd02f5ec097a732e07d86268402e4da50
Marie Roald <roald.marie@gmail.com>
2022-10-21 07:13:09 +0200

    Bump version: 0.1.4 → 0.1.5

--- src/matcouply/__init__.py
+++ src/matcouply/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Marie Roald"""
 __email__ = "roald.marie@gmail.com"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 
 from . import coupled_matrices, data, decomposition, penalties, random
